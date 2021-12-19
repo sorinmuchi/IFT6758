0e3e0d75e8539d2072bf788e91c259fa0ec75c1d
Sorin Muchi <smuchi@drw.com>
2021-12-18 21:06:46 -0500

    Cleanup again

--- build.sh
+++ build.sh
@@ -1,4 +1,2 @@
 #!/bin/bash
-
 docker build -t ift6758/serving:1.0.0 -f Dockerfile.serving .
-# docker-compose build
--- run.sh
+++ run.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-
 docker run -it -p 6758:6758 --env-file .env ift6758/serving:1.0.0
-# docker-compose up
